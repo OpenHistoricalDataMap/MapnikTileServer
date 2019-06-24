d0cd66f209f7bad1b779dcb68ab7a6bcb5f227e2
dsfreak93 <dsfreak93@googlemail.com>
2019-06-24 13:41:36 +0200

    test für app ordner gebaut

---
+++ tile_server/test/test_generate_default_style_xml.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+import redis
+from flask import Flask
+
+from tile_server.code import app
+
+
+class TestGenerate_default_style_xml(TestCase):
+    app = Flask(__name__)
+    cache = redis.Redis(host='redis', port=6379)
+
+    def test_generate_default_style_xml(self):
+        assert app.generate_default_style_xml() == ""
+
