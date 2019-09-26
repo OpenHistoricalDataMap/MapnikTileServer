23b0792f407b9ca6cc396329c3523d29e27cc35a
Steffen Exler <steffen.exler@gmail.com>
2019-09-26 11:25:28 +0200

    update admin panel for ohdm app

--- ohdm_django_mapnik/ohdm/admin.py
+++ ohdm_django_mapnik/ohdm/admin.py
@@ -8,8 +8,21 @@ from .models import (
     PlanetOsmRoads,
 )
 
-admin.site.register(TileCache)
-admin.site.register(PlanetOsmLine)
-admin.site.register(PlanetOsmPoint)
-admin.site.register(PlanetOsmPolygon)
-admin.site.register(PlanetOsmRoads)
+
+class TileCacheAdmin(admin.ModelAdmin):
+    list_display = ('celery_task_id', 'created', 'celery_task_done')
+    list_filter = ['celery_task_done']
+    search_fields = ['celery_task_id', 'created', 'valid_since', 'valid_until', 'zoom', 'x_pixel', 'y_pixel']
+
+
+class PlanetOsmAdmin(admin.ModelAdmin):
+    list_display = ('osm_id', 'tags', 'valid_since', 'valid_until')
+    list_filter = ['valid_since', 'valid_until']
+    search_fields = ['osm_id']
+
+
+admin.site.register(TileCache, TileCacheAdmin)
+admin.site.register(PlanetOsmLine, PlanetOsmAdmin)
+admin.site.register(PlanetOsmPoint, PlanetOsmAdmin)
+admin.site.register(PlanetOsmPolygon, PlanetOsmAdmin)
+admin.site.register(PlanetOsmRoads, PlanetOsmAdmin)
