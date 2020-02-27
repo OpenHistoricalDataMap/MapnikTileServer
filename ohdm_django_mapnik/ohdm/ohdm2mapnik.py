56f15d86deb4d1969232b927e0cc682566fa82dc
Steffen Exler <fubu@v2202002103215109785.hotsrv.de>
2020-02-27 13:39:10 +0100

    add multithreading for ohdm2mapnik

--- ohdm_django_mapnik/ohdm/ohdm2mapnik.py
+++ ohdm_django_mapnik/ohdm/ohdm2mapnik.py
@@ -1,4 +1,6 @@
+import multiprocessing
 import time
+from multiprocessing.dummy import Pool as ThreadPool
 from typing import Any, List, Optional, Tuple
 
 from ohdm_django_mapnik.ohdm.models import (
@@ -48,7 +50,7 @@ class Ohdm2Mapnik:
     -> https://wiki.openstreetmap.org/wiki/Osm2pgsql/schema
     """
 
-    def __init__(self, bulk_amount: int = 100000, sql_chunk_size: int = 100000):
+    def __init__(self, bulk_amount: int = 1000, sql_chunk_size: int = 1000):
         """
         setup Ohdm2Mapnik class
         
@@ -75,6 +77,9 @@ class Ohdm2Mapnik:
 
         # process start time
         self.start_time: float = 0
+
+        # already added objects ids (useful to not add double objects)
+        self.done_objects: List[int] = []
 
     def drop_planet_tables(self):
         """
@@ -144,6 +149,12 @@ class Ohdm2Mapnik:
         display process time
         """
 
+        self.row = (
+            PlanetOsmLine.objects.all().count()
+            + PlanetOsmPoint.objects.all().count()
+            + PlanetOsmPolygon.objects.all().count()
+        )
+
         process_time: float = time.time() - self.start_time
         percent: float = self.row / self.estimate_total_rows * 100
 
@@ -173,51 +184,32 @@ class Ohdm2Mapnik:
         Arguments:
             planet_object {Any} -- PlanetOsmLine, PlanetOsmPoint or PlanetOsmPolygon object witch should be added to cache & db
         """
-        self.row += 1
+        # self.row += 1
 
-        self.planet_object_to_cache(planet_object=planet_object)
-        # planet_object.save()
+        # self.planet_object_to_cache(planet_object=planet_object)
+        planet_object.save()
 
-        if self.row % self.bulk_amount == 0:
-            self.save_planet_object_cache()
+        # if self.row % self.bulk_amount == 0:
+        #     self.save_planet_object_cache()
 
-        if self.row % 1000 == 0:
-            self.display_process_time()
+        # print(self.row)
 
-    def run(self):
-        """
-        convert ohdm database to mapnik readable tables
-        """
-        self.start_time = time.time()
+        # if self.row % 1000 == 0:
+        #     self.display_process_time()
 
-        print("Delete old mapnik tables!")
-        self.drop_planet_tables()
-
-        # calc estimate amount of rows
-        self.estimate_total_rows = (
-            OhdmLines.objects.all().count()
-            + OhdmPolygons.objects.all().count()
-            + OhdmPoints.objects.all().count()
-        )
-
-        print("Start converting of {} entries!".format(self.estimate_total_rows))
-
-        # iterate through every ohdm entry
-        for geoobject in OhdmGeoobject.objects.all().iterator(
-            chunk_size=self.sql_chunk_size
-        ):
-
+    def convert(self, range_begin: int):
+        for geoobject in OhdmGeoobject.objects.all()[
+            range_begin : range_begin + self.sql_chunk_size
+        ]:
             if not geoobject.name:
-
-                done_objects: List[int] = []
 
                 # load all geoobject_geometry for geoobject
                 for geoobject_geometry in OhdmGeoobjectGeometry.objects.filter(
                     id_geoobject_source=geoobject
                 ):
                     # jump already added objects
-                    if geoobject_geometry.id in done_objects:
-                        continue
+                    if geoobject_geometry.id in self.done_objects:
+                        return
 
                     planet_object: Optional[
                         Any
@@ -226,7 +218,7 @@ class Ohdm2Mapnik:
                     # continue if planet_object not exists
                     if not planet_object:
                         print("{} is no valid geometry object!".format(geoobject.id))
-                        continue
+                        return
 
                     planet_object.name = geoobject.name
                     planet_object.geoobject = geoobject
@@ -244,7 +236,7 @@ class Ohdm2Mapnik:
                             id_target=geoobject_geometry.id_target,
                             type_target=geoobject_geometry.type_target,
                         ):
-                            done_objects.append(geoobject_geometry_sub.id)
+                            self.done_objects.append(geoobject_geometry_sub.id)
                             planet_object = self.set_classification(
                                 planet_object=planet_object,
                                 classification_id=geoobject_geometry_sub.classification_id,
@@ -274,7 +266,7 @@ class Ohdm2Mapnik:
                             geoobject.id, geoobject.name
                         )
                     )
-                    continue
+                    return
 
                 # fill planet_object with base values
                 planet_object.name = geoobject.name
@@ -288,5 +280,41 @@ class Ohdm2Mapnik:
 
                 self.count_row(planet_object=planet_object)
 
+        self.display_process_time()
+
+    def run(self, threads: int = multiprocessing.cpu_count()):
+        """
+        convert ohdm database to mapnik readable tables
+        
+        Keyword Arguments:
+            threads {int} -- how many threads should use for processing (default: number of CPU cores of host system)
+        """
+        self.start_time = time.time()
+
+        self.done_objects.clear()
+
+        print("Delete old mapnik tables!")
+        self.drop_planet_tables()
+
+        # calc estimate amount of rows
+        self.estimate_total_rows = (
+            OhdmLines.objects.all().count()
+            + OhdmPolygons.objects.all().count()
+            + OhdmPoints.objects.all().count()
+        )
+
+        print("Start converting of {} entries!".format(self.estimate_total_rows))
+
+        # iterate through every ohdm entry
+        rows: int = OhdmGeoobject.objects.all().count()
+        work_ids: List[int] = []
+        for row in range(0, rows, self.sql_chunk_size):
+            work_ids.append(row)
+
+        pool: ThreadPool = ThreadPool(threads)
+        pool.map(self.convert, work_ids)
+        pool.close()
+        pool.join()
+
         # save last objects
-        self.save_planet_object_cache()
+        # self.save_planet_object_cache()
