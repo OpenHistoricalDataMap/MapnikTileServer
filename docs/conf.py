90553ab22a9534b993004a57d77da3e0e8417a42
Steffen Exler <fubu@v2202002103215109785.hotsrv.de>
2020-03-30 09:26:01 +0200

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = "0.1.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo']
+extensions = ["sphinx.ext.todo"]
 
 numfig = True  # enable numfig
 
