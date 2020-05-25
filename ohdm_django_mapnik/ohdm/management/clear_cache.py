ecb6c53cd0c7d8326c5be2d09137e8c69e8b36c0
Steffen Exler <Steffen.Exler@pm.me>
2020-05-25 19:12:10 +0200

    remove unused file

--- ohdm_django_mapnik/ohdm/management/clear_cache.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    help = "Clear-Up Cache"
-
-    def handle(self, *args, **options):
-        print("comming soon...")
