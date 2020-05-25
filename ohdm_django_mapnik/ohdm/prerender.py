ff9ad3cce96530552461c0a18889a477f4f85c36
Steffen Exler <Steffen.Exler@pm.me>
2020-05-25 13:33:34 +0200

    fix typing

--- ohdm_django_mapnik/ohdm/prerender.py
+++ ohdm_django_mapnik/ohdm/prerender.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+from typing import Optional
 
 from config.settings.base import OSM_CARTO_STYLE_XML, env
 from ohdm_django_mapnik.ohdm.models import (
@@ -11,40 +12,60 @@ from ohdm_django_mapnik.ohdm.tasks import async_generate_tile
 
 def prerender(zoom_level: int):
     # get the lowest valid_since for each geometry
-    point_valid_since: PlanetOsmPoint = PlanetOsmPoint.objects.all().order_by(
+    point_valid_since: Optional[date] = PlanetOsmPoint.objects.all().order_by(
         "valid_since"
-    )[0]
-    line_valid_since: PlanetOsmLine = PlanetOsmLine.objects.all().order_by(
+    )[0].valid_since
+    line_valid_since: Optional[date] = PlanetOsmLine.objects.all().order_by(
         "valid_since"
-    )[0]
-    polygon_valid_since: PlanetOsmPolygon = PlanetOsmPolygon.objects.all().order_by(
+    )[0].valid_since
+    polygon_valid_since: Optional[date] = PlanetOsmPolygon.objects.all().order_by(
         "valid_since"
-    )[0]
+    )[0].valid_since
 
     # get the highest valid_until for each geometry
-    point_valid_until: PlanetOsmPoint = PlanetOsmPoint.objects.all().order_by(
+    point_valid_until: Optional[date] = PlanetOsmPoint.objects.all().order_by(
         "-valid_until"
-    )[0]
-    line_valid_until: PlanetOsmLine = PlanetOsmLine.objects.all().order_by(
+    )[0].valid_until
+    line_valid_until: Optional[date] = PlanetOsmLine.objects.all().order_by(
         "-valid_until"
-    )[0]
-    polygon_valid_until: PlanetOsmPolygon = PlanetOsmPolygon.objects.all().order_by(
+    )[0].valid_until
+    polygon_valid_until: Optional[date] = PlanetOsmPolygon.objects.all().order_by(
         "-valid_until"
-    )[0]
+    )[0].valid_until
 
     # get the lowest valid_since for all geometries
-    valid_since: date = point_valid_since.valid_since
-    if line_valid_since.valid_since < valid_since:
-        valid_since = line_valid_since.valid_since
-    if polygon_valid_since.valid_since < valid_since:
-        valid_since = polygon_valid_since.valid_since
+    valid_since: date
+    if point_valid_since:
+        valid_since = point_valid_since
+
+    if line_valid_since:
+        if not valid_since:
+            valid_since = line_valid_since
+        if line_valid_since < valid_since:
+            valid_since = line_valid_since
+
+    if polygon_valid_since:
+        if not valid_since:
+            valid_since = polygon_valid_since
+        if polygon_valid_since < valid_since:
+            valid_since = polygon_valid_since
 
     # get the highest valid_until for all geometries
-    valid_until: date = point_valid_until.valid_until
-    if line_valid_until.valid_until < valid_until:
-        valid_until = line_valid_until.valid_until
-    if polygon_valid_until.valid_until < valid_since:
-        valid_until = polygon_valid_until.valid_until
+    valid_until: date
+    if point_valid_until:
+        valid_until = point_valid_until
+
+    if line_valid_until:
+        if not valid_until:
+            valid_until = line_valid_until
+        if line_valid_until < valid_until:
+            valid_until = line_valid_until
+
+    if polygon_valid_until:
+        if not valid_until:
+            valid_until = polygon_valid_until
+        if polygon_valid_until < valid_until:
+            valid_until = polygon_valid_until
 
     print("Start prerender")
     delta = timedelta(days=1)
